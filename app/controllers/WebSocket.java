33984cfa2fe2e03c73ee53dfb85a639eb44a251e
Matt Hall <matt@larvalabs.com>
2015-12-22 11:22:26 -0500

    User server side auth for websocket.

--- app/controllers/WebSocket.java
+++ app/controllers/WebSocket.java
@@ -15,67 +15,77 @@ import java.util.*;
 import models.*;
 
 public class WebSocket extends Controller {
-    
+
     public static void room(String user) {
         render(user);
     }
 
     public static class ChatRoomSocket extends WebSocketController {
-        
-        public static void join(String user) {
-            
+
+        public static void join() {
+
+            ChatUser user;
+            if (session.contains("uid")) {
+                Logger.info("websocket found existing user: " + session.get("uid"));
+                user = ChatUser.get(Long.parseLong(session.get("uid")));
+            } else {
+                Logger.info("no user id in session.");
+                disconnect();
+                return;
+            }
+
             ChatRoom room = ChatRoom.get();
-            
+
             // Socket connected, join the chat room
-            EventStream<ChatRoom.Event> roomMessagesStream = room.join(user);
-         
+            EventStream<ChatRoom.Event> roomMessagesStream = room.join(user.username);
+
             // Loop while the socket is open
-            while(inbound.isOpen()) {
-                
+            while (inbound.isOpen()) {
+
                 // Wait for an event (either something coming on the inbound socket channel, or ChatRoom messages)
-                Either<WebSocketEvent,ChatRoom.Event> e = await(Promise.waitEither(
-                    inbound.nextEvent(), 
-                    roomMessagesStream.nextEvent()
+                Either<WebSocketEvent, ChatRoom.Event> e = await(Promise.waitEither(
+                        inbound.nextEvent(),
+                        roomMessagesStream.nextEvent()
                 ));
-                
+
                 // Case: User typed 'quit'
-                for(String userMessage: TextFrame.and(Equals("quit")).match(e._1)) {
-                    room.leave(user);
+                for (String userMessage : TextFrame.and(Equals("quit")).match(e._1)) {
+                    room.leave(user.username);
                     outbound.send("quit:ok");
                     disconnect();
                 }
-                
+
                 // Case: TextEvent received on the socket
-                for(String userMessage: TextFrame.match(e._1)) {
-                    room.say(user, userMessage);
+                for (String userMessage : TextFrame.match(e._1)) {
+                    room.say(user.username, userMessage);
                 }
-                
+
                 // Case: Someone joined the room
-                for(ChatRoom.Join joined: ClassOf(ChatRoom.Join.class).match(e._2)) {
+                for (ChatRoom.Join joined : ClassOf(ChatRoom.Join.class).match(e._2)) {
                     outbound.send("join:%s", joined.user);
                 }
-                
+
                 // Case: New message on the chat room
-                for(ChatRoom.Message message: ClassOf(ChatRoom.Message.class).match(e._2)) {
+                for (ChatRoom.Message message : ClassOf(ChatRoom.Message.class).match(e._2)) {
                     outbound.send("message:%s:%s", message.user, message.text);
                 }
-                
+
                 // Case: Someone left the room
-                for(ChatRoom.Leave left: ClassOf(ChatRoom.Leave.class).match(e._2)) {
+                for (ChatRoom.Leave left : ClassOf(ChatRoom.Leave.class).match(e._2)) {
                     outbound.send("leave:%s", left.user);
                 }
-                
+
                 // Case: The socket has been closed
-                for(WebSocketClose closed: SocketClosed.match(e._1)) {
-                    room.leave(user);
+                for (WebSocketClose closed : SocketClosed.match(e._1)) {
+                    room.leave(user.username);
                     disconnect();
                 }
-                
+
             }
-            
+
         }
-        
+
     }
-    
+
 }
 
