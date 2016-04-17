73169898842d8f6a75a6e86e8bd7acdd7a0f8d59
Rick Hanlon II <rickhanlonii@gmail.com>
2016-04-17 16:07:04 -0400

    Add webpack prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,11 @@
+var webpack = require('webpack');
+
 module.exports = {
+  devtool: 'cheap-module-source-map',
   entry: "./web/index.js",
   output: {
     filename: "./public/dist/bundle.js"
   },
-  watch:true,
   module: {
     loaders: [
       {
@@ -19,4 +21,16 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx', '.es6']
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      }
+    }),
+  ]
 };
