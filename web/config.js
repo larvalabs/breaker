1f8350e3fe21aea31540a3002b9c1396a21ec60a
Rick Hanlon II <rickhanlonii@gmail.com>
2016-04-19 15:23:23 -0400

    Fix room header + mod customize link

--- web/config.js
+++ web/config.js
@@ -6,7 +6,8 @@ let Config = {
     prod: window.__ENVIRONMENT__ == "production"
   },
   settings: {
-    ping_timeout: 20000
+    ping_timeout: 20000,
+    default_banner: "Message from the moderators to you, the user."
   },
   websocket_url: window.__WEBSOCKET_URL__,
   room_name: window.__ROOM_NAME__
