7b52774501f64b9d28835c06f3a22042a9c5ae1c
Rick Hanlon II <rickhanlonii@gmail.com>
2016-05-01 13:41:49 -0400

    Close larvalabs/breakerweb#94

--- web/userlist/UserAvatar.jsx
+++ web/userlist/UserAvatar.jsx
@@ -7,10 +7,13 @@ export default class UserAvatar extends Component {
     if(Config.features.useFlairStyle(this.props.roomName)){
       return null
     }
-
+    let userImage = this.props.user.get('profileImageUrl');
+    if(!userImage){
+      userImage = "/public/img/user-anon.png";
+    }
     let userLink = `https://reddit.com/u/${this.props.user.get('username')}`;
     return <a href={userLink} className="pull-left thumb-sm avatar m-r" target="_blank">
-      <img src={this.props.user.get('profileImageUrl')} alt="..." className="img-circle"/>
+      <img src={userImage} alt="..." className="img-circle"/>
       <i className={ (this.props.user.get('online') ? 'on' : 'off' ) + " b-white bottom"} />
     </a>
   }
