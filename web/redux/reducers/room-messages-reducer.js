d04599e60183956bc10f3c92cdce29219c3fe2ec
Rick Hanlon II <rickhanlonii@gmail.com>
2016-05-09 18:51:26 -0400

    Handle system messages in redux

--- web/redux/reducers/room-messages-reducer.js
+++ web/redux/reducers/room-messages-reducer.js
@@ -4,11 +4,7 @@ import Immutable from 'immutable'
 
 export default function roomMessages(state=Immutable.Map(), action) {
   switch (action.type) {
-    case (socketTypes.SOCK_SERVER): {
-      // TODO: there's no UUID for these
-      return state;
-      // return state.set(action.message.room.name, state.get(action.message.room.name, Immutable.List()).push(message.uuid));
-    }
+    case (socketTypes.SOCK_SERVER):
     case (socketTypes.SOCK_MESSAGE): {
       return state.set(action.message.room.name, state.get(action.message.room.name, Immutable.List()).push(action.message.message.uuid));
     }
