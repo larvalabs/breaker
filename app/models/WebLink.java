76d8ba87c80e7b8fe86a8c74528ad815a50dab5b
Matt Hall <matt@larvalabs.com>
2016-05-11 15:47:55 -0400

    Type, Site and imageSize fields for linkInfo. Don't include fields which are null or -1. Related to #135.

--- app/models/WebLink.java
+++ app/models/WebLink.java
@@ -16,11 +16,14 @@ public class WebLink extends Model {
 
     @Unique
     private String url;
+    private String type;
+    private String site;
     private String title;
     private String description;
     private String imageUrl;
-    private int imageWidth;
-    private int imageHeight;
+    private int imageWidth = -1;
+    private int imageHeight = -1;
+    private long imageSize = -1;
     private String videoUrl;
     private String videoType;
     private int videoWidth = -1;
@@ -28,23 +31,41 @@ public class WebLink extends Model {
 
     public WebLink(String url, LinkInfo linkInfo) {
         this.url = url;
+        this.type = linkInfo.getType();
+        this.site = linkInfo.getSite();
         this.title = linkInfo.getTitle();
         this.description = linkInfo.getDescription();
         this.imageUrl = linkInfo.getImageUrl();
-        this.imageWidth = linkInfo.getImageWidth();
-        this.imageHeight = linkInfo.getImageHeight();
+        if (linkInfo.getImageWidth() != null) {
+            this.imageWidth = linkInfo.getImageWidth();
+        }
+        if (linkInfo.getImageHeight() != null) {
+            this.imageHeight = linkInfo.getImageHeight();
+        }
+        if (linkInfo.getImageSize() != null) {
+            this.imageSize = linkInfo.getImageSize();
+        }
         this.videoUrl = linkInfo.getVideoUrl();
-        this.videoWidth = linkInfo.getVideoWidth();
-        this.videoHeight = linkInfo.getVideoHeight();
+        if (linkInfo.getVideoWidth() != null) {
+            this.videoWidth = linkInfo.getVideoWidth();
+        }
+        if (linkInfo.getVideoHeight() != null) {
+            this.videoHeight = linkInfo.getVideoHeight();
+        }
     }
 
-    public WebLink(String url, String title, String description, String imageUrl, int imageWidth, int imageHeight, String videoUrl, String videoType, int videoWidth, int videoHeight) {
+    public WebLink(String url, String type, String site, String title, String description,
+                   String imageUrl, int imageWidth, int imageHeight, long imageSize,
+                   String videoUrl, String videoType, int videoWidth, int videoHeight) {
         this.url = url;
+        this.type = type;
+        this.site = site;
         this.title = title;
         this.description = description;
         this.imageUrl = imageUrl;
         this.imageWidth = imageWidth;
         this.imageHeight = imageHeight;
+        this.imageSize = imageSize;
         this.videoUrl = videoUrl;
         this.videoType = videoType;
         this.videoWidth = videoWidth;
@@ -57,6 +78,22 @@ public class WebLink extends Model {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
     }
 
     public String getTitle() {
@@ -131,17 +168,38 @@ public class WebLink extends Model {
         this.videoHeight = videoHeight;
     }
 
+    public long getImageSize() {
+        return imageSize;
+    }
+
+    public void setImageSize(long imageSize) {
+        this.imageSize = imageSize;
+    }
+
     public LinkInfo getLinkInfo() {
         LinkInfo info = new LinkInfo();
         info.setUrl(url);
+        info.setType(type);
+        info.setSite(site);
         info.setTitle(title);
         info.setDescription(description);
         info.setImageUrl(imageUrl);
-        info.setImageWidth(imageWidth);
-        info.setImageHeight(imageHeight);
+        if (imageWidth > 0) {
+            info.setImageWidth(imageWidth);
+        }
+        if (imageHeight > 0) {
+            info.setImageHeight(imageHeight);
+        }
+        if (imageSize > 0) {
+            info.setImageSize(imageSize);
+        }
         info.setVideoUrl(videoUrl);
-        info.setVideoWidth(videoWidth);
-        info.setVideoHeight(videoHeight);
+        if (videoWidth > 0) {
+            info.setVideoWidth(videoWidth);
+        }
+        if (videoHeight > 0) {
+            info.setVideoHeight(videoHeight);
+        }
         return info;
     }
 }
