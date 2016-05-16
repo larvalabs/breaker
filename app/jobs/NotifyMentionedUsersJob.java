a9dc76814c9f007688f397a5c3a594139cbf58e0
Matt Hall <matt@larvalabs.com>
2016-05-16 13:16:46 -0400

    Don't show notification optout message when user is online. Fixes #190.

--- app/jobs/NotifyMentionedUsersJob.java
+++ app/jobs/NotifyMentionedUsersJob.java
@@ -49,27 +49,28 @@ public class NotifyMentionedUsersJob extends Job {
             for (ChatUser mentionedUser : mentionedUsers) {
                 Logger.info("User mentioned: " + mentionedUser.getUsername() + " in message id " + message.getId());
                 ChatRoomStream eventStream = ChatRoomStream.getEventStream(message.getRoom().getName());
-                if (mentionedUser.isNotificationEnabledForMention() && !allOnlineUsersForAllRooms.contains(mentionedUser.getUsername())
-                        && !OptOutUser.didOptOut(mentionedUser.getUsername())) {
+                if (mentionedUser.isNotificationEnabledForMention() && !OptOutUser.didOptOut(mentionedUser.getUsername())) {
 
-                    String subject = "You were mentioned in "+chatRoom.getName() + " chat";
-                    String content = "/u/" + message.getUser().getUsername() + " mentioned you in " + chatRoom.getName() + " chat:\n\n "
-                            + "\"" + message.getMessageText() + "\"\n\n"
-                            + "To respond: " + url + "\n\n"
-                            + "To opt out of any future messages: https://www.breakerapp.com/optout/" + message.getUuid() + "/" + mentionedUser.getUsername();
-                    try {
-                        RedditUtil.sendPrivateMessageFromBot(mentionedUser.getUsername(), subject, content);
-                        eventStream.sayFromServer(JsonChatRoom.from(chatRoom, chatRoom.getModeratorUsernames()), message.getUser().getUsername(),
-                                "User " + mentionedUser.getUsername() + " is offline, so we notified them via PM on Reddit.");
-                        Stats.count(Stats.StatKey.REDDIT_PM_SUCCESS, 1);
-                    } catch (NetworkException | ApiException e) {
-                        eventStream.sayFromServer(JsonChatRoom.from(chatRoom, chatRoom.getModeratorUsernames()), message.getUser().getUsername(),
-                                "Failed to notify " + mentionedUser.getUsername() + " via Reddit PM, please notify an admin.");
-                        Stats.count(Stats.StatKey.REDDIT_PM_FAILED, 1);
-                        e.printStackTrace();
+                    if (!allOnlineUsersForAllRooms.contains(mentionedUser.getUsername())) {
+                        String subject = "You were mentioned in " + chatRoom.getName() + " chat";
+                        String content = "/u/" + message.getUser().getUsername() + " mentioned you in " + chatRoom.getName() + " chat:\n\n "
+                                + "\"" + message.getMessageText() + "\"\n\n"
+                                + "To respond: " + url + "\n\n"
+                                + "To opt out of any future messages: https://www.breakerapp.com/optout/" + message.getUuid() + "/" + mentionedUser.getUsername();
+                        try {
+                            RedditUtil.sendPrivateMessageFromBot(mentionedUser.getUsername(), subject, content);
+                            eventStream.sayFromServer(JsonChatRoom.from(chatRoom, chatRoom.getModeratorUsernames()), message.getUser().getUsername(),
+                                    "User " + mentionedUser.getUsername() + " is offline, so we notified them via PM on Reddit.");
+                            Stats.count(Stats.StatKey.REDDIT_PM_SUCCESS, 1);
+                        } catch (NetworkException | ApiException e) {
+                            eventStream.sayFromServer(JsonChatRoom.from(chatRoom, chatRoom.getModeratorUsernames()), message.getUser().getUsername(),
+                                    "Failed to notify " + mentionedUser.getUsername() + " via Reddit PM, please notify an admin.");
+                            Stats.count(Stats.StatKey.REDDIT_PM_FAILED, 1);
+                            e.printStackTrace();
+                        }
                     }
                 } else {
-                    Logger.info("Not notifying mentioned user "+mentionedUser.getUsername()+" because of notification preference or because they are online.");
+                    Logger.info("Not notifying mentioned user " + mentionedUser.getUsername() + " because of notification preference or because they are online.");
                     eventStream.sayFromServer(JsonChatRoom.from(chatRoom, chatRoom.getModeratorUsernames()), message.getUser().getUsername(),
                             "Not notifying " + mentionedUser.getUsername() + " via Reddit PM, they have requested to not receive PMs.");
                 }
