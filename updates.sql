2f6e6a41ef1d1555d23558b0105532b3759de0b0
Matt Hall <matt@larvalabs.com>
2016-05-20 16:42:56 -0400

    Deployed database changes. Related to #183.

--- updates.sql
+++ updates.sql
@@ -4,8 +4,9 @@ alter table chatuser add column admin boolean not null default false;
 -- New messages storage
 alter table userroom add column lastSeenMessageTime int8 default 0;
 
--- ALL DEPLOYED ABOVE
-
 -- displayName official storage
+alter table chatroom add COLUMN displayname VARCHAR(255);
 update chatroom set displayname=name;
 update chatroom set name=LOWER(displayname);
+
+-- ALL DEPLOYED ABOVE
