5b0a50b5b8e7c01e9885ed679c832c79836aa18b
Rick Hanlon II <rickhanlonii@gmail.com>
2016-05-24 16:25:53 -0400

    Add urlfurling for all HTML5 videos

--- web/links/Video.jsx
+++ web/links/Video.jsx
@@ -1,14 +1,17 @@
 import React, {Component} from 'react'
 import Immutable from 'immutable'
-import VideoYouTube from './VideoYouTube'
+import VideoHTML5 from './VideoHTML5'
 
 export default class Article extends Component {
   constructor(props){
     super(props);
   }
   render(){
-    if(this.props.linkInfo.get('site', '').toLocaleLowerCase() === 'youtube'){
+    const site = this.props.linkInfo.get('site', '').toLocaleLowerCase();
+    if(site === 'youtube'){
       return <VideoYouTube linkInfo={this.props.linkInfo} />
+    } else if(site){
+      return <VideoHTML5 linkInfo={this.props.linkInfo} />
     }
     
     return null;
