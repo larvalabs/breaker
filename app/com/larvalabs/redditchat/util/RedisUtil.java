6927ccd7bd77bee9a101053b0ffb20ebbd21a976
Matt Hall <matt@larvalabs.com>
2016-06-04 00:03:02 -0400

    Reduce timing stats recording for redis user presence stat. Reduce frequency of presence list cleanup. Related to #9.

--- app/com/larvalabs/redditchat/util/RedisUtil.java
+++ app/com/larvalabs/redditchat/util/RedisUtil.java
@@ -18,7 +18,7 @@ import java.util.TreeSet;
 public class RedisUtil {
 
     public static final String REDISKEY_PRESENCE_GLOBAL = "presence__global";
-    public static final double CHANCE_CLEAN_REDIS_PRESENCE = 0.1;
+    public static final double CHANCE_CLEAN_REDIS_PRESENCE = 0.01;
 
     private static Random random = new Random();
 
@@ -52,12 +52,14 @@ public class RedisUtil {
             long startTime = System.currentTimeMillis();
             int time = (int) (System.currentTimeMillis() / 1000);
             Redis.zadd(getRedisPresenceKeyForRoom(roomName), time, getUsernameAndConnectionString(username, connectionId));
+            userPresentGlobal(username, connectionId);
             if (random.nextFloat() < CHANCE_CLEAN_REDIS_PRESENCE) {
                 // this is just housekeeping to keep the sets from getting too big
                 cleanPresenceSet(roomName);
+
+                // Just occasionally measure timing for this to avoid overloading logs
+                Stats.measure(Stats.StatKey.REDIS_TIMING_USERPRESENT, (System.currentTimeMillis() - startTime));
             }
-            userPresentGlobal(username, connectionId);
-            Stats.measure(Stats.StatKey.REDIS_TIMING_USERPRESENT, (System.currentTimeMillis() - startTime));
         } catch (Exception e) {
             Logger.error(e, "Error contacting redis.");
         }
