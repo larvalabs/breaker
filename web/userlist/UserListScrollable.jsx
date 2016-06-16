5ce4a6bd551b40bcff9bab58153925ffe3ccbe3f
Rick Hanlon II <rickhanlonii@gmail.com>
2016-06-16 10:39:29 -0400

    Remove col class, inline styles instead
    Closes larvalabs/breaker#19

--- web/userlist/UserListScrollable.jsx
+++ web/userlist/UserListScrollable.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 
 export default class UserListScrollable extends Component {
   render() {
+    const styles = {
+      display: 'table-cell',
+      float: 'none',
+      height: '100%',
+      verticalAlign: 'top'
+    };
+
     return (
-      <div className="col w-md lter b-l hidden-sm hidden-xs">
+      <div className="w-md lter b-l hidden-sm hidden-xs" style={styles}>
         <div className="vbox">
           <div className="row-row">
             <div className="cell scrollable hover">
