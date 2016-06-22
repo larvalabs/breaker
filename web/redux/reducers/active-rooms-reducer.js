1ff05958b680914b11ee6f8d2899e43e03bd2b73
KenavR <kenavr@gmail.com>
2016-06-23 01:09:48 +0200

    Changed the receiving structure of additional active channels

--- web/redux/reducers/active-rooms-reducer.js
+++ web/redux/reducers/active-rooms-reducer.js
@@ -5,11 +5,7 @@ import * as activeRoomsTypes from '../constants/active-rooms-contants';
 export default function activeRooms(state = Immutable.Map(), action) {
   switch (action.type) {
     case (activeRoomsTypes.ACTIVE_ROOMS_LOADED_MORE): {
-      return state.merge(Immutable.fromJS(action.activeRooms.reduce((obj, room) => {
-        const newObj = obj;
-        newObj[room.name] = room;
-        return newObj;
-      }, {})));
+      return state.merge(Immutable.fromJS(action.activeRooms));
     }
     default:
       return state;
