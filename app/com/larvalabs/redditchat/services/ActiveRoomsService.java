bcbcf4aaf2c726c92a7a4734b53cb6850eb57ed8
Matt Hall <matt@larvalabs.com>
2016-06-23 15:46:02 -0400

    Don't allow negative offset in active rooms query.

--- app/com/larvalabs/redditchat/services/ActiveRoomsService.java
+++ app/com/larvalabs/redditchat/services/ActiveRoomsService.java
@@ -37,7 +37,7 @@ public class ActiveRoomsService {
             .append("SELECT cr.id AS id, cr.name AS name, cr.displayname AS displayName, cr.iconurl AS iconUrl, COUNT(DISTINCT user_id) AS activeUsers, RANK() OVER (ORDER BY COUNT(DISTINCT user_id) DESC, name) as rank ")
             .append("FROM chatroom cr ")
             .append("LEFT JOIN message m on cr.id = m.room_id ")
-            .append("WHERE m.createdate > (current_timestamp - interval '3 days') AND ")
+            .append("WHERE m.createdate > (current_timestamp - interval '30 days') AND ")
             .append("m.user_id <> (SELECT id FROM chatuser WHERE username = :botname) ")
             .append("GROUP BY cr.id, cr.name, cr.displayname, cr.iconurl ")
             .append("ORDER BY activeUsers DESC ")
@@ -52,7 +52,11 @@ public class ActiveRoomsService {
 
         Query activeRoomsQuery = JPA.em().createNativeQuery(sb.toString());
         activeRoomsQuery.setParameter("limit", limit);
-        offset = (offset != 0) ? offset-1 : offset;
+        if (offset > 0) {
+            offset -= -1;
+        } else if (offset < 0) {
+            offset = 0;
+        }
         activeRoomsQuery.setParameter("offset", offset);
         userId = (userId != null) ? userId : -1;
         activeRoomsQuery.setParameter("userId", userId);
